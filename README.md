a7b69154623b93f1d04828f6eedb7473bfa5f1f0
Burton Smith <burton@breakstuff.io>
2022-01-31 07:44:49 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Create FAST Element Starter
+
+This is CLI tool for building your own web component library using [FAST Element](https://www.fast.design/docs/fast-element/getting-started).
+
+## Getting Started
+
---
+++ src/index.ts
@@ -0,0 +1,57 @@
+const { red, green, yellow, underline, bold } = require('colorette');
+const fs = require('fs');
+const download = require("download-git-repo");
+
+async function run() {
+    const args = process.argv.slice(2);
+    const projectName = args[0];
+
+    try {
+        validateProjectName(projectName, args);
+        await createApp(projectName);
+    } catch (error: any) {
+        console.error(red(`\n${error.message}\n`));
+        process.exit(1);
+    }
+}
+
+async function createApp(projectName: string) {
+    await download('break-stuff/fast-element-starter', `${projectName}`, function (err: any) {
+        if(err) {
+            console.error(red(`There was an error downloading the repository: \n\n${err.message}`));
+        }
+    });
+
+    logInstructions(projectName);
+}
+
+function validateProjectName(projectName: string, args: string[]) {
+    if(args.length > 1) {
+        throw new Error(`Project name "${args.join(' ')}" is not valid and must be a kebab-case name without spaces.`);
+    }
+
+    if (!isValidateProjectName(projectName)) {
+        throw new Error(`Project name "${projectName}" is not valid and must be a kebab-case name without spaces.`);
+    }
+
+    if (fs.existsSync(projectName)) {
+        throw new Error(`Folder "./${projectName}" already exists, please choose a different project name.`);
+    }
+}
+
+function isValidateProjectName(projectName: string) {
+    return !/[^a-z0-9-]/.test(projectName);
+}
+
+function logInstructions(projectName: string) {
+    console.log(`
+    ${green('Fast Element Starter successfully downloaded!')}
+
+    ${underline(bold('Next steps:'))}
+    cd ${projectName}
+    npm install
+    npm run dev
+    `);
+}
+
+run();
